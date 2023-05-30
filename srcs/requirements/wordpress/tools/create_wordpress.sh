a852ef16d5efba5bf7bf5246cead410d430ef1a5
remi <r.fourault@gmail.com>
2023-05-30 10:52:08 +0200

    finished mandatory + redis

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -2,11 +2,11 @@
 
 if [ -f ./wp-config.php ]
 then
-	echo 'wordpress already downloaded"
+	echo "wordpress already downloaded"
 else
 
 	#download wordpress
-	wget http://wordpress.org/latest.tar.gz
+	wget https://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
@@ -18,6 +18,15 @@ else
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
+
+	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+ 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+  	#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+ 	wp config set WP_REDIS_CLIENT phpredis --allow-root
+	wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+	wp redis enable --allow-root
 
 fi
 

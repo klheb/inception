31eb3ad34b93429eac49fc3caf86093775683911
klheb <r.fourault@gmail.com>
2024-04-23 17:11:42 +0200

    update mariadb script for mariadb10.5 without EOF usage

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -11,28 +11,19 @@ then
 	echo "database already exist"
 else
 
-	#launch installation
-	mysql_secure_installation << _EOF_
+    #launch installation
+    echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'; DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;" | mysql -u root
 
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
+    #Add a root user on 127.0.0.1 to allow remote connexion 
+    #Flush privileges allow to your sql tables to be updated automatically when you modify it
+    #mysql -uroot launch mysql command line client
+    echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
 
-	#Add a root user on 127.0.0.1 to allow remote connexion 
-	#Flush privileges allow to your sql tables to be updated automatically when you modify it
-	#mysql -uroot launch mysql command line client
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+    #create database and user
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"| mysql -u root
 
-	#create database and user
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"| mysql -u root
-
-	#import database
-	mysql -u root -p $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+    #import database
+    mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 

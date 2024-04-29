700a935c10e497f6fc9cd916a5b4294228abd2f1
klheb <r.fourault@gmail.com>
2024-04-29 15:50:39 +0200

    ca marche la

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -17,7 +17,7 @@ else
     mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -e "FLUSH PRIVILEGES;"
 
     sleep 1

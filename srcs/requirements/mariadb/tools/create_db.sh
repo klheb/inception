1d611ad9695991c498d354cf6baaccfc800e8810
klheb <r.fourault@gmail.com>
2024-04-29 16:45:26 +0200

    ca marche la

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 set -e
 
-# if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
+if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]
 
-# then
-#     echo "${MYSQL_DATABASE} already exists\n"
+then
+    echo "${MYSQL_DATABASE} already exists\n"
 
-# else 
-    # Start MariaDB service
-service mariadb start
-# Wait for MariaDB to start
-sleep 5
-# Log into MariaDB as root and execute SQL commands
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
-sleep 1
+else 
+        # Start MariaDB service
+    service mariadb start
+    # Wait for MariaDB to start
+    sleep 5
+    # Log into MariaDB as root and execute SQL commands
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -e "FLUSH PRIVILEGES;"
+    sleep 1
 
-echo "STARTING MARIADB"
+    echo "STARTING MARIADB"
 
-# Stop MariaDB service
-mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
-sleep 1
-# Print status
-echo "MariaDB database and user were created successfully!"
+    # Stop MariaDB service
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    sleep 1
+    # Print status
+    echo "MariaDB database and user were created successfully!"
 
-# fi
+fi
 # Start MariaDB in the foreground
 
 exec mysqld --debug
